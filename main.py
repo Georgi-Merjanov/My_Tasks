a25dcd6348b67469b1a34be935705d8aaffd711a
Georgi-Merjanov <georgi.merdjanov10@gmail.com>
2026-05-27 18:49:09 +0300

    models.py

--- main.py
+++ main.py
@@ -1,45 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Integer, Boolean, ForeignKey, Date
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_migrate import Migrate
 from password import password
+from models import db, User, Task
 from datetime import date, timedelta
 import os
 
 app=Flask(__name__)
 app.secret_key="taen_kluch"
 
-class Base(DeclarativeBase):
-    pass
-
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://postgres:{password}@localhost:5432/python_project_tasks"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
-db=SQLAlchemy(model_class=Base)
 db.init_app(app)
 migrate=Migrate(app, db)
-
-
-class User(Base):
-    __tablename__ = "Users"
-    id: Mapped[int]=mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(255), nullable=False)
-    profile_picture: Mapped[str] = mapped_column(String(255), nullable=True)
-    tasks: Mapped[list["Task"]] = relationship("Task", back_populates="user", cascade="all, delete-orphan")
-
-
-class Task(Base):
-    __tablename__ = "Tasks"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(200), nullable=False)
-    day_for: Mapped[date] = mapped_column(Date, nullable=False)
-    is_finished: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("Users.id", name="fk_task_user"), nullable=False)
-    user: Mapped["User"] = relationship("User", back_populates="tasks")
-
 
 with app.app_context():
     db.create_all()
---
+++ models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, Integer, Boolean, ForeignKey, Date
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
+from datetime import date
+
+class Base(DeclarativeBase):
+    pass
+
+db=SQLAlchemy(model_class=Base)
+
+class User(Base):
+    __tablename__ = "Users"
+    id: Mapped[int]=mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    profile_picture: Mapped[str] = mapped_column(String(255), nullable=True)
+    tasks: Mapped[list["Task"]] = relationship("Task", back_populates="user", cascade="all, delete-orphan")
+
+class Task(Base):
+    __tablename__ = "Tasks"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    day_for: Mapped[date] = mapped_column(Date, nullable=False)
+    is_finished: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("Users.id", name="fk_task_user"), nullable=False)
+    user: Mapped["User"] = relationship("User", back_populates="tasks")
